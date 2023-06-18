a63fd073b16cff0cd2695a6f1b52c604b4576712
maherrera603 <maherrera603@misena.edu.co>
2023-06-17 19:24:10 -0500

    error solution

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,19 +1,1 @@
-<<<<<<< HEAD
 export const url: string = "https://central-l4ub.onrender.com/api/"
-=======
-import * as CryptoJS from "crypto-js";
-
-export const url: string = "http://localhost:8000/api/"
-const key:string = "pruebafinal";
-
-export const encrypt = (data:string):string => {
-  return CryptoJS.AES.encrypt(data, key).toString();
-}
-
-export const desencrypt = (data:string|null):string|null => {
-  if (data != null){
-    return CryptoJS.AES.decrypt(data, key).toString();
-  }
-  return null;
-}
->>>>>>> developer
